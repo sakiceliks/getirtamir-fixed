b4a48120787f167ae7d30bbffa436dc7fd7bf832
Saki Celik <87222451+sakiceliks@users.noreply.github.com>
2024-04-26 19:02:46 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+            plugins: [
+              [
+                '@babel/plugin-transform-react-jsx',
+                {
+                  throwIfNamespace: false,
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      fontFamily: {
+        figtree: ['var(--font-figtree)'],
+      },
+    },
+  },
+  plugins: [],
+}
